18af2da84d4520ff35b06f2565c3a9bbf87f4129
Bazerche21 <149426550+Bazerche21@users.noreply.github.com>
2024-03-03 11:52:30 +0100

    Add files via upload

---
+++ Homework #1/Homework #1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int firstNumber = 10;
+int secondNumber = 15;
+int thirdNumber = 20;
+int fourthNumber = 30;
+
+int total = firstNumber + secondNumber + thirdNumber + fourthNumber;
+int average = total / 4;
+
+Console.WriteLine("The average of " +firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber + " is:" + average);
+
+
+
---
+++ Homework#1 - Task 2/Homework#1 - Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int firstNumber, secondNumber, temp;
+
+Console.WriteLine("Enter first number: ");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+ temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+Console.WriteLine("After Swapping:");
+Console.WriteLine("First Number: " + firstNumber);
+Console.WriteLine("Second Number: " + secondNumber);
