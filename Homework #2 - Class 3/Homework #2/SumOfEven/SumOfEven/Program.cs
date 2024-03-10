b697a3ec046e5415f119c0ac892915b7db74345e
Bazerche21 <149426550+Bazerche21@users.noreply.github.com>
2024-03-10 15:21:25 +0100

    Add files via upload

---
+++ Homework #2 - Class 3/Homework #2/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+int[] numbers = new int[6];
+
+Console.WriteLine("Enter 6 numbers: ");
+
+for (int i = 0; i < 6; i++)
+{
+    Console.Write($"Number {i + 1}: ");
+    if (int.TryParse(Console.ReadLine(), out int number))
+    {
+        numbers[i] = number;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input! Please enter an integer.");
+        i--;
+    }
+
+    int sumOfEven = 0;
+
+    foreach(int num in numbers)
+    {
+        if (num %2 == 0) {
+
+            sumOfEven+= num;
+        }
+    }
+
+    Console.WriteLine($"Sum of even numbers: {sumOfEven}");
+}
