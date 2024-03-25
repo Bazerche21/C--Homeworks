688cc28cbe670d0fe58e73021e11d062769e1005
Bazerche21 <149426550+Bazerche21@users.noreply.github.com>
2024-03-25 23:44:53 +0100

    Add files via upload

---
+++ Homework#7 - Class 9/Homework - Class 9/LINQ - Exercise/Program.cs
@@ -0,0 +1,8 @@
+﻿List<int> numbers = new List<int> {4, 8, 7, 9, 13, 14, 18, 24, 27, 30};
+
+List<int> squaresOfEvens = numbers.Where(x => x % 2 == 0).Select(x => x * x).ToList();
+
+Console.WriteLine("Original Numbers:");
+Console.WriteLine(string.Join(", ", numbers));
+Console.WriteLine("\nSquares of Even Numbers:");
+Console.WriteLine(string.Join(", ", squaresOfEvens));
